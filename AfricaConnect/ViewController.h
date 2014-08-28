0ec5af8623a2704cab6a25c32c23f467a6c279e0
Mohamed Jalloh <RIADSALIHINE@mohameds-mbp.home>
2014-08-27 22:54:00 -0400

    Initial Commit

---
+++ AfricaConnect/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AfricaConnect
+//
+//  Created by Mohamed Jalloh on 8/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AfricaConnectTests/AfricaConnectTests.m
@@ -0,0 +1,34 @@
+//
+//  AfricaConnectTests.m
+//  AfricaConnectTests
+//
+//  Created by Mohamed Jalloh on 8/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AfricaConnectTests : XCTestCase
+
+@end
+
+@implementation AfricaConnectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
